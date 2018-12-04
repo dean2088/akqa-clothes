cfb27690c507282e676eb06d19d206609a9fe9cf
dean2088 <jason.huang@bigtincan.com>
2018-12-04 11:37:11 +1100

    update css

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -4,16 +4,14 @@ import styles from './style.less';
 const ProductItem = ({ imageLink, productName, price, isSale, isExclusive }) => {
   return (
     <div className={styles.ProductItem}>
+      <img src={imageLink} alt={productName} />
       <div>
-        <img src={imageLink} alt={productName} />
-        <div>
-          {isSale && <div className={styles.sale}>{'Sale'}</div>}
-          {isExclusive && <div className={styles.exclusive}>{'Exclusive'}</div>}
-        </div>
-        <div className={styles.info}>
-          <div>{productName}</div>
-          <div>{price}</div>
-        </div>
+        {isSale && <div className={styles.sale}>{'Sale'}</div>}
+        {isExclusive && <div className={styles.exclusive}>{'Exclusive'}</div>}
+      </div>
+      <div className={styles.info}>
+        <div>{productName}</div>
+        <div>{price}</div>
       </div>
     </div>
   );
